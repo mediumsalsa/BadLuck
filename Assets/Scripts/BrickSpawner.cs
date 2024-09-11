216c0d8766c3dee635be137a8bfb05282cd9fd9b
Caleb Wolthers <w0473807@nscc.ca>
2024-09-11 15:16:43 -0300

    Brick Spawner

--- Assets/Scripts/BrickSpawner.cs
+++ Assets/Scripts/BrickSpawner.cs
@@ -5,9 +5,30 @@ using UnityEngine;
 public class BrickSpawner : MonoBehaviour
 {
     public GameObject brick;
+    public int brickSpawnRate;
+    private int tillNextBrick;
+    private Vector2 brickSpawn;
 
     private void Start()
     {
-        Instantiate(brick);
+        tillNextBrick = 0;
     }
+
+    private void Update()
+    {
+        int brickRandomiser = Random.Range(-10, 10);
+
+        brickSpawn = gameObject.transform.position;
+
+        if (tillNextBrick >= brickSpawnRate)
+        {
+            tillNextBrick = 0;
+            Instantiate(brick, new Vector2(brickSpawn.x + brickRandomiser, brickSpawn.y), Quaternion.identity);
+
+        } else {
+            tillNextBrick++;
+        }
+
+    }
+
 }
